b516d6df42fb0aae7eaa6bb7af2546482646886d
lombardmatt <lombardm@gilmou.org>
2020-09-25 10:40:57 -0400

    Update student.py

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
         
-        self.cha-cha()
+        self.cha-cha(
         #self.sprinkler()
         #self.dance(1)
         #self.dance(2)
