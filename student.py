345d56f46a48957985b86519d315f0ddce7bcb16
lombardmatt <lombardm@gilmou.org>
2020-09-29 11:15:14 -0400

    Update student.py

--- student.py
+++ student.py
@@ -96,7 +96,7 @@ class Piggy(PiggyParent):
         for x in range(3):
             self.right()
             time.sleep(1)
-       self.stop()
+        self.stop()
 
     def example_move(self):
         """this is an example dance move that should be replaced by student-created content"""
