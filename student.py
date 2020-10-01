524ad549b4d0b35fe43bf22c68e59f16940bd84e
lombardmatt <lombardm@gilmou.org>
2020-10-01 10:36:40 -0400

    Update student.py

--- student.py
+++ student.py
@@ -108,9 +108,11 @@ class Piggy(PiggyParent):
 
 
     def dance1(self):
+        time.sleep(1)
         self.fwd()
+        time.sleep(1)
         self.right()
-        time.sleep(.5)
+        time.sleep(1)
         self.stop()
         time.sleep(.5)
         self.back()
