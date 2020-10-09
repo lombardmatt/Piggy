bf8e864abe841bf3c1f6875f3af0494cef5ade7a
lombardmatt <lombardm@gilmou.org>
2020-10-09 11:22:10 -0400

    adj to scan i

--- student.py
+++ student.py
@@ -320,11 +320,21 @@ class Piggy(PiggyParent):
 
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
+        for s in range(4):
+            self.obstacle_scan()
+            self.turn_by_deg(90)
+        self.stop()
+        
+        
+        
+        
+    def obstacle_scan(self):
         # scan
         self.scan()
         # FIGURE OUT HOW MANY obejcts THERE WERE
         see_an_object = False
         count = 0
+
         for angle in self.scan_data:
             dist = self.scan_data[angle]
             if dist < self.SAFE_DISTANCE and not see_an_object:
