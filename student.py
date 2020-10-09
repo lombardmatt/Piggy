6b31e70392966e9dcb78d8d2b444606f4f4e48da
lombardmatt <lombardm@gilmou.org>
2020-10-09 11:18:53 -0400

    adding loop

--- student.py
+++ student.py
@@ -321,24 +321,25 @@ class Piggy(PiggyParent):
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
         # scan
-        self.scan()
+        for x in range(4):
+            self.scan()
         # FIGURE OUT HOW MANY obejcts THERE WERE
         see_an_object = False
         count = 0
 
-        for angle in self.scan_data(4):
-            dist = self.scan_data[angle]
-            if dist < self.SAFE_DISTANCE and not see_an_object:
-                see_an_object = True
-                count += 1
-                print("I see something")
-            elif dist > self.SAFE_DISTANCE and see_an_object:
-                see_an_object = False
-                print("The object is no more")
+            for angle in self.scan_data:
+                dist = self.scan_data[angle]
+                if dist < self.SAFE_DISTANCE and not see_an_object:
+                    see_an_object = True
+                    count += 1
+                    print("I see something")
+                elif dist > self.SAFE_DISTANCE and see_an_object:
+                    see_an_object = False
+                    print("The object is no more")
+                print("ANGLE %d | DIST: %d" % (angle,dist))
+            print("\nI saw %d objects" % count)
             self.turn_by_deg(90)
-            time.sleep(1)
-            print("ANGLE %d | DIST: %d" % (angle,dist))
-        print("\nI saw %d objects" % count)
+    
 
 
 
