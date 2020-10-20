1e32327d3e42ba15d9722160415e492d2113142b
lombardmatt <lombardm@gilmou.org>
2020-10-20 11:00:48 -0400

    making turn count from 3 to 2

--- student.py
+++ student.py
@@ -439,7 +439,7 @@ class Piggy(PiggyParent):
 
                 # turn count code taken from Parker Strauss and slightly modified by yours truly
                 """ if the robot is stuck, it will turn 3 times and scan, and if there is no safe place to leave it will go out the way it came """
-                if turn_count > 3:
+                if turn_count > 2:
                     self.turn_by_deg(135)
                     self.stop()
                     time.sleep(1)
